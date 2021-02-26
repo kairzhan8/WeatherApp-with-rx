3a2ce712657e00f3e383b61a972b54d0a58fae4e
kairzhan <kairzhankural@gmail.com>
2021-02-26 17:32:25 +0600

    interface

--- WeatherApp with rx/ViewController.swift
+++ WeatherApp with rx/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel:  UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
 
 
