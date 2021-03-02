43de335d6b62852756164db68bc08347a70f3558
kairzhan <kairzhankural@gmail.com>
2021-03-02 13:27:33 +0600

    using driver instead of bind

--- WeatherApp with rx/ViewController.swift
+++ WeatherApp with rx/ViewController.swift
@@ -43,14 +43,14 @@ class ViewController: UIViewController {
         
         let search = URLRequest.load(resource: resource)
             .observe(on: MainScheduler.instance)
-            .catchAndReturn(WeatherResult.empty)
+            .asDriver(onErrorJustReturn: WeatherResult.empty)
             
         search.map { "\($0.main.temp) ℃"}
-            .bind(to: self.temperatureLabel.rx.text)
+            .drive(self.temperatureLabel.rx.text)
             .disposed(by: disposeBag)
         
         search.map { "\($0.main.humidity)  💦"}
-            .bind(to: self.humidityLabel.rx.text)
+            .drive(self.humidityLabel.rx.text)
             .disposed(by: disposeBag)
         
     }
